6ef392986bc6b0950d57ae6af90dd95876509e7e
arrblair <ryan@livefyre.com>
2013-07-12 18:01:34 -0700

    add templater shell script to repo

---
+++ cli_html_templater.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -e '<!DOCTYPE html>\n<html>\n    <head>\n        <title>' > template.html;
+echo -e '\n        </title>\n    </head>\n    <body>\n    </body>\n</html>' >> template.html
