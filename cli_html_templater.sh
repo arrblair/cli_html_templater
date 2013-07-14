2032c225e0dd136a985c769f93125c659f147518
arrblair <ryan@livefyre.com>
2013-07-13 23:35:03 -0700

    add conditionals checking for presence of index.html and if true, prompt for overwrite

--- cli_html_templater.sh
+++ cli_html_templater.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo -e '<!DOCTYPE html>\n<html>\n    <head>\n        <title>' > index.html
-echo -e '\n        </title>\n    </head>\n    <body>\n    </body>\n</html>' >> index.html
+if [ -f "index.html" ]
+then
+  read -p "There's already an index.html in this directory; overwrite? " -r
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    echo -e '<!DOCTYPE html>\n<html>\n    <head>\n        <title>' > index.html
+    echo -e '\n        </title>\n    </head>\n    <body>\n\n    </body>\n</html>' >> index.html
+  fi
+fi
