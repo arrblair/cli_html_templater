1cb39f7ecbaf71ad3c3ddf6029c8cdcfe0a1562f
arrblair <ryan@livefyre.com>
2013-07-14 00:19:06 -0700

    build out regex for responses and else to conditional that issues response when user decides not to overwrite existent index.html

--- cli_html_templater.sh
+++ cli_html_templater.sh
@@ -3,9 +3,11 @@
 if [ -f "index.html" ]
 then
   read -p "There's already an index.html in this directory; overwrite? " -r
-  if [[ $REPLY =~ ^[Yy]$ ]]
+  if [[ $REPLY =~ [Yy]|Yes|yes ]]
   then
     echo -e '<!DOCTYPE html>\n<html>\n    <head>\n        <title>' > index.html
     echo -e '\n        </title>\n    </head>\n    <body>\n\n    </body>\n</html>' >> index.html
+  else
+    echo 'Not overwriting existent index.html.'
   fi
 fi
